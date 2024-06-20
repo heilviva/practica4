84b7d6fa9da5c7fe47316537acd19f822ce2fe24
heilviva <145266521+heilviva@users.noreply.github.com>
2024-06-20 16:09:41 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Button List Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setLayout(new FlowLayout());
+
+        String[] groupNames = {"Т50-11-23", "П50-11-23", "Т50-11-22"};
+
+        for (String groupName : groupNames) {
+            JButton button = new JButton(groupName);
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JFrame groupFrame = new JFrame("Group Info");
+                    JLabel label = new JLabel(groupName, JLabel.CENTER);
+                    groupFrame.add(label);
+                    groupFrame.setSize(200, 100);
+                    groupFrame.setVisible(true);
+                }
+            });
+            frame.add(button);
         }
+
+        frame.setVisible(true);
     }
 }
